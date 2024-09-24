95a469afa024cc9315ca0e9ea8581e07e5a294f8
YuktiKamthan <yuktikamthan@gmail.com>
2024-09-24 13:15:48 +0530

    Initial commit

---
+++ src/test/java/com/example/BasicConceptsRefresh/BasicConceptsRefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BasicConceptsRefresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicConceptsRefreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
